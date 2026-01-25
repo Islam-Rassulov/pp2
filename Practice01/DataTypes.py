e744241c72401694eb01954be636313ed2af55be
Islam-Rassulov <isko.rasulov@bk.ru>
2026-01-25 14:44:53 +0500

    DataTypes

---
+++ Practice01/DataTypes.py
@@ -0,0 +1,12 @@
+#Example 1 Getting the data type
+x = 5
+print(type(x))
+
+#Example 2 Setting the data type
+d = 20
+e = ["apple", "banana", "cherry"]
+f = 1j
+#Example 3 Setting the specific data type
+a = int(20)
+b = list(("apple", "banana", "cherry"))
+c = complex(1j)
