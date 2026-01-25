223335e673e05ec44a2d5031952e98fc06200525
Islam-Rassulov <isko.rasulov@bk.ru>
2026-01-25 15:36:35 +0500

    string

---
+++ Practice01/String.py
@@ -0,0 +1,30 @@
+#Example 1
+print("Hello")
+print('Hello')
+
+#Example 2 Strings are Array
+a = "Hello, World!"
+print(a[1])
+
+#Example 3 Looping Through String
+for x in "banana":
+  print(x)
+
+#Example 4 String Length
+b = "Hello, World!"
+print(len(b))
+
+#Example 5 Check String
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+#Example 6 Slicing String 
+b = "Hello, World!"
+print(b[2:5])
+
+c = "Hello, World!"
+print(b[-5:-2])
