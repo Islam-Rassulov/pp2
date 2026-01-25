4154f4c7fcfb24049f07864210c99301787fa29b
Islam-Rassulov <isko.rasulov@bk.ru>
2026-01-25 14:53:13 +0500

    Casting

---
+++ Practice01/Casting.py
@@ -0,0 +1,15 @@
+#Example 1 
+a = int(1)   # x will be 1
+b = int(2.8) # y will be 2
+c = int("3") # z will be 3
+
+#Example 2
+d = float(1)     # x will be 1.0
+e = float(2.8)   # y will be 2.8
+f = float("3")   # z will be 3.0
+g = float("4.2") # w will be 4.2
+
+#Example 3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
