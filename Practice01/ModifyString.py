52b8aae0e19b76c20b6a60616ad2247ef30814d5
Islam-Rassulov <isko.rasulov@bk.ru>
2026-01-27 11:28:04 +0500

    boolean

---
+++ Practice01/ModifyString.py
@@ -0,0 +1,21 @@
+#Example 1 UpperCase
+a = "Hello, World!"
+print(a.upper())
+
+#Example 2 LowerCase
+a = "Hello, World!"
+print(a.lower())
+
+#Example 3 Remove WhiteSpace
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+#Example 4 Replace String
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Example 5 String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
